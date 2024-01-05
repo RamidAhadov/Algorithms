5f8ef80b2e9ff2f9e0a590063c2ddc2aef02e71a
RamidAhadov <ramidehedov02@gmail.com>
2024-01-06 02:10:54 +0400

    QuickSort and MergeSort folders added

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,10 @@
+namespace Algorithms.MergeSort;
+
+public class MergeSort<T> where T:IComparable
+{
+    public List<T> Sort(T[] array)
+    {
+        var result = new List<T>();
+        return result;
+    }
+}
